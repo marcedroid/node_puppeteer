21e72321e51a8d651263758e8c0fbeb9fe9e6899
marcelo <marcelo.tk3@gmail.com>
2017-09-18 16:24:25 -0500

    Default & viewports

---
+++ index.js
@@ -0,0 +1,67 @@
+const puppeteer = require('puppeteer');
+
+const URI = process.env.URI || 'https://www.instagram.com/wwe/';
+const filename = Math.random().toString(36).substring(2,7);
+const action = process.env.ACTION || null;
+
+/*
+ * Default
+ */
+let do_default = ()=>{
+    (async()=>{
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.goto(URI);
+
+        await page.screenshot({
+            path:`shots/${filename}.png`,
+            fullPage:true
+        });
+
+        browser.close();
+    })();
+};
+
+/*
+ * Viewports
+ */
+let do_viewports = ()=>{
+    (async()=>{
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        const viewports = [1600,1024,720,460];
+
+        await page.goto(URI);
+
+        for (let i=0; i<viewports.length; i++){
+            let vw = viewports[i];
+
+            await page.setViewport({
+                width:vw,
+                height:1000
+            });
+
+            await page.screenshot({
+                path:`shots/${filename}-${vw}.png`,
+                fullPage:true
+            })
+        }
+
+        browser.close();
+    })();
+};
+
+
+
+/*
+ * RUN CODE
+ */
+switch (action){
+    case 'viewports':
+        do_viewports();
+        break;
+    default:
+        do_default();
+}
